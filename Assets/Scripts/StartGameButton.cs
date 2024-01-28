9a97197a9ce5a098d22eaca504be8b42d5bf2187
Kyle Doerksen <60625560+KyleD123@users.noreply.github.com>
2024-01-27 20:30:36 -0600

    Updated start screen button

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartGameButton : MonoBehaviour
 {
-    public UnityEngine.UI.Button btn;
+    public Button btn;
+    public Sprite onHoverSprite;
+    public Sprite exitHoverSprite;
+
 
     private void Start()
     {
+
         btn.onClick.AddListener(() => { StartGame(); });
+
+    }
+
+    public void OnMouseEnter()
+    {
+        btn.gameObject.GetComponent<Image>().sprite = onHoverSprite;
+    }
+
+    public void OnMouseExit()
+    {
+        btn.gameObject.GetComponent<Image>().sprite = exitHoverSprite;
     }
 
     public void StartGame()
